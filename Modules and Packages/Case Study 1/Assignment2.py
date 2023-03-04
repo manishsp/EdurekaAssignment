b57830e595ec0e91f951866722189fed7158dba1
ManishPoojari <pmanish009@gmail.com>
2023-03-04 21:54:40 +0530

    Assignment 2 completed from modules and packages case study 1

--- Modules and Packages/Case Study 1/Assignment2.py
+++ Modules and Packages/Case Study 1/Assignment2.py
@@ -2,19 +2,41 @@
 #     specific data from that list.
 # Hint: Use if/elif to deal with conditions.
 
-mylist = [1, 56, 'sa', 67, 'ga', 8, 're']
+# Section of code  to sort the list with both numeric and non-numeric
+
+mylist = ['z', 1, 56, 'sa', 67, 'ga', 8, 're']
+
 numberlist = []
 alphalist = []
-alphanum = []
-print(len(mylist))
-i = 0
-while i < len(mylist):
-    if mylist[i].isnumeric():
-        numberlist.append(mylist[i])
-    elif i.isalpha():
-        alphalist.append(i)
-    else:
-        alphanum.append(i)
-    i = +1
 
-print(f'numbers :{numberlist}, alphabets:{alphalist}, alphanumeric: {alphanum}')
+ctr = 0
+for i in mylist:
+    try:
+        if i.isnumeric():
+            numberlist.append(i)
+        elif i.isalpha():
+            alphalist.append(i)
+
+        ctr += 1
+        if ctr == len(mylist):
+            break
+        else:
+            pass
+    except AttributeError:
+        numberlist.append(i)
+
+print(f'numbers :{numberlist}, alphabets:{alphalist}')
+mylist2 = sorted(numberlist) + sorted(alphalist)  # sort both the new lists separately and add it new list
+print(mylist2)
+# Section of search a specific data from new sorted list
+searchinput = input("Enter the data to be searched in the company list\n ====> ")
+
+try:
+    if searchinput in mylist2:
+        print(f"{searchinput} is found in the list")
+    elif int(searchinput) in mylist2:
+        print(f"{searchinput} is found in the list")
+    else:
+        print(f"{searchinput} is not found in the list")
+except ValueError:
+    print(f"{searchinput} is not found in the list")
