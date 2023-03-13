702df7f594ad4e4428513eedcb73d26c538e0d80
MspDx <pmanish009@gmail.com>
2023-03-13 15:41:49 +0530

    Assignment12.py completed

---
+++ Modules and Packages/Case Study 1/Assignment12.py
@@ -0,0 +1,13 @@
+# Write a program that accepts a sequence of whitespace separated words as input and prints the words after removing
+# all duplicate words and sorting them alphanumerically. Suppose the following input is supplied to the program:
+# hello world and practice makes perfect and hello world again Then, the output should be: again and hello makes
+# perfect practice world
+
+input_set = set(input("Enter your sentence: ").split())
+mylist=list(input_set)
+mylist.sort()
+
+print("\n\n ----Output----\n ")
+for i in range(len(mylist)):
+    print(mylist[i], end=" ")
+
