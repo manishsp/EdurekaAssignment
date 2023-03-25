e71d632dcb7e5fcd527071ddb4a709b7e35c8534
MspDx <pmanish009@gmail.com>
2023-03-25 16:46:01 +0530

    Module and Packages Assignment15.py completed

--- Modules and Packages/Case Study 1/Assignment15.py
+++ Modules and Packages/Case Study 1/Assignment15.py
@@ -1,4 +1,12 @@
 # 15. Give example of fsum and sum function of math library
 
-#example is to find the perce
+# example is to find the total marks of a student
+
 import math
+
+marks_list = list(map(float, input("Please enter 4 subjects marks separated by space:").split(",")))
+
+print(marks_list)
+print(f"Total Marks via fsum: {math.fsum(marks_list)}")
+
+print(f"Total Marks via sum: {sum(marks_list)}")
