23a0685b473a8192e3c4ec41ebb9fd1a30e1d7e0
MspDx <pmanish009@gmail.com>
2023-04-09 20:02:49 +0530

    Case Study 3 Merger.py in progress

--- Modules and Packages/Case Study 3/Merger.py
+++ Modules and Packages/Case Study 3/Merger.py
@@ -1,6 +1,7 @@
 import struct
 
-import Customer, re
+import Customer
+import re
 
 # fh=open('C:\\Users\\pmani\\IdeaProjects\\EdurekaAssignments\\Modules and Packages\\Case Study 3\\FairDealCustomerData.csv')
 fh = open('/Users/MspDx/IdeaProjects/EdurekaAssignment/Modules and Packages/Case Study 3/FairDealCustomerData.csv')
@@ -8,18 +9,32 @@ fh = open('/Users/MspDx/IdeaProjects/EdurekaAssignment/Modules and Packages/Case
 # print(fh.read())
 fullnamelist = []
 blacklistvalue = []
+customer_m = Customer.Customer()
 for x in fh:
-    x=re.sub('\s+',' ',x)
+    x = re.sub('\s+', ' ', x)
     # x = x.strip('\n')
     mylist = str.split(x, ',')
+    mylist[1]=str.strip(mylist[1])
     fullnamelist.append(str.strip(mylist[1]))
     blacklistvalue.append(mylist[2])
+    splitname = re.split("\s", mylist[1])
+    if len(splitname)==3:
+        customer_m.setFname(splitname[1])
+        customer_m.setTitle(splitname[0])
+        customer_m.setLname(splitname[2])
+    else:
+        customer_m.setFname(splitname[1])
+        customer_m.setTitle(splitname[0])
+        customer_m.setLname("")
+
+    print(customer_m.getTitle())
+    print(customer_m.getFname())
+    print(customer_m.getLname())
     # mydict = {"fullname": fullnamelist, "blacklist": blacklistvalue}
 
 fh.close()
-print(f"fullname list length: {len(fullnamelist)} and blacklist list length: {len(blacklistvalue)}")
-
-# print(fullnamelist[21])
-# print(blacklistvalue[21])
-
-# print(fullnamelist.index("Mr. William John"))
+#
+# print(splitname)
+# print(customer_m.getTitle())
+# print(customer_m.getFname())
+# print(customer_m.getLname())
