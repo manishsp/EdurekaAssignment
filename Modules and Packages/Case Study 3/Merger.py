d45ac5df2fe927af6049edd9f325f7e3e8547262
MspDx <pmanish009@gmail.com>
2023-04-10 13:40:10 +0530

    Case Study 3 Merger.py in progress

--- Modules and Packages/Case Study 3/Merger.py
+++ Modules and Packages/Case Study 3/Merger.py
@@ -1,7 +1,13 @@
-import struct
+import Order
 
 import Customer
 import re
+
+
+class CustomerNotAllowedException(Exception):
+    def __init__(self,value):
+        self.value=self
+
 
 # fh=open('C:\\Users\\pmani\\IdeaProjects\\EdurekaAssignments\\Modules and Packages\\Case Study 3\\FairDealCustomerData.csv')
 fh = open('/Users/MspDx/IdeaProjects/EdurekaAssignment/Modules and Packages/Case Study 3/FairDealCustomerData.csv')
@@ -10,15 +16,15 @@ fh = open('/Users/MspDx/IdeaProjects/EdurekaAssignment/Modules and Packages/Case
 fullnamelist = []
 blacklistvalue = []
 customer_m = Customer.Customer()
+# ord1= Order.Order()
 for x in fh:
     x = re.sub('\s+', ' ', x)
-    # x = x.strip('\n')
     mylist = str.split(x, ',')
-    mylist[1]=str.strip(mylist[1])
+    mylist[1] = str.strip(mylist[1])
     fullnamelist.append(str.strip(mylist[1]))
     blacklistvalue.append(mylist[2])
     splitname = re.split("\s", mylist[1])
-    if len(splitname)==3:
+    if len(splitname) == 3:
         customer_m.setFname(splitname[1])
         customer_m.setTitle(splitname[0])
         customer_m.setLname(splitname[2])
@@ -26,19 +32,27 @@ for x in fh:
         customer_m.setFname(splitname[1])
         customer_m.setTitle(splitname[0])
         customer_m.setLname("")
-    customer_m.setIsblacklisted(mylist[2])
-    print(customer_m.isblacklisted)
+    customer_m.setIsblacklisted(int(mylist[2]))
 
-    print(customer_m.getTitle())
-    print(customer_m.getFname())
-    print(customer_m.getLname())
-    # mydict = {"fullname": fullnamelist, "blacklist": blacklistvalue}
+    # print(f"{customer_m.getFname()} {customer_m.getLname()}: {customer_m.isblacklisted}")
+
+    try:
+        if customer_m.isblacklisted == 0:
+            print(f"Customer: {customer_m.getFname()} {customer_m.getLname()}is not blacklisted")
+        else:
+            raise CustomerNotAllowedException("Exception")
+    except CustomerNotAllowedException:
+        print(f"Customer: {customer_m.getFname()} {customer_m.getLname()} is blacklisted and not allowed")
+
+Order.Order.createOrder("William",1,"XXXX","yyy")
+
+    # def createOrder(y,z):
+    #     if z==1:
+    #         raise CustomerNotAllowedException
+    #     else:
+    #         print(f"order created for {y}")
+    # createOrder(customer_m.getFname(),customer_m.isblacklisted)
 
 fh.close()
 
-
-#
-# print(splitname)
-# print(customer_m.getTitle())
-# print(customer_m.getFname())
-# print(customer_m.getLname())
+Order.Order.createOrder()
---
+++ Modules and Packages/Case Study 3/Order.py
@@ -0,0 +1,21 @@
+import Merger
+
+
+class Order(Merger.CustomerNotAllowedException):
+
+    def __init__(self):
+        self.value = self
+
+    def __str__(self):
+        pass
+
+    def createOrder(self, customerfname, blacklistvalue, productname, productcode):
+        self.customerfname = customerfname
+        self.blacklistvalue = blacklistvalue
+        self.productname = productname
+        self.productcode = productcode
+
+        if blacklistvalue == 1:
+            raise Merger.CustomerNotAllowedException
+        else:
+            print(f"{customerfname} is allowed")
