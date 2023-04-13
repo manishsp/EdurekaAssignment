241f6fe9b2494238eb39af5f22d9855891eb2642
ManishPoojari <pmanish009@gmail.com>
2023-04-13 21:21:52 +0530

    Numpy-Pandas Case Study -Assign1 completed

--- NumPy and Pandas/Case Study 1/Assignment 1.py
+++ NumPy and Pandas/Case Study 1/Assignment 1.py
@@ -2,6 +2,13 @@
 # column in a separate NumPy array
 
 import numpy as np
+import pandas as pd
 
-arr=np.loadtxt('C:\\Users\\pmani\\IdeaProjects\\EdurekaAssignment\\NumPy and Pandas\\CSV\\SalaryGender.csv',delimiter=",",dtype=str)
-print(arr)
+df = pd.read_csv('C:\\Users\\pmani\\IdeaProjects\\EdurekaAssignment\\NumPy and Pandas\\CSV\\SalaryGender.csv')
+
+salary_arr = np.array(df["Salary"])
+gender_arr = np.array(df["Gender"])
+age_arr = np.array(df["Age"])
+phd_arr = np.array(df["PhD"])
+
+print(salary_arr, "\n\n", gender_arr, "\n\n", age_arr, "\n\n", phd_arr, "\n\n")
