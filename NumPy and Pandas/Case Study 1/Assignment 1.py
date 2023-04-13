441505b2b245de8b781d1606621bda020ebca40b
ManishPoojari <pmanish009@gmail.com>
2023-04-13 23:06:47 +0530

    Numpy-Pandas Case Study 1 -Solution 2 completed

--- NumPy and Pandas/Case Study 1/Assignment 1.py
+++ NumPy and Pandas/Case Study 1/Assignment 1.py
@@ -1,5 +1,5 @@
-# Extract data from the given SalaryGender CSV file and store the data from each
-# column in a separate NumPy array
+# 1.# Extract data from the given SalaryGender CSV file and store the data from each
+# # column in a separate NumPy array
 
 import numpy as np
 import pandas as pd
@@ -11,4 +11,18 @@ gender_arr = np.array(df["Gender"])
 age_arr = np.array(df["Age"])
 phd_arr = np.array(df["PhD"])
 
-print(salary_arr, "\n\n", gender_arr, "\n\n", age_arr, "\n\n", phd_arr, "\n\n")
+print("*** Solution for 1 *** \n",salary_arr, "\n\n", gender_arr, "\n\n", age_arr, "\n\n", phd_arr, "\n\n")
+
+
+# 2. Find:
+# 1. The number of men with a PhD --assumed gender value as 1
+# 2. The number of women with a PhD --assumed gender value as 0
+
+
+df3=df[(df['Gender'] == 1) & (df['PhD'] == 1)] #assumed gender value as 1
+df4=df[(df['Gender'] == 0) & (df['PhD'] == 1)] #assumed gender value as 0
+womenwithphd=df4['Gender'].count()
+menwithphd=df3['Gender'].count()
+
+print (f"*** Solution for 2 *** \nMen with PhD: {menwithphd} \nWomen with PhD: {womenwithphd} ")
+
