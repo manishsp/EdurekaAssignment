2cbc7ec309f78bbbd3627dbc72a5a07c453f6686
MspDx <pmanish009@gmail.com>
2023-04-17 13:20:49 +0530

    Numpy & Panda Case Study1-Assignment6 started

--- NumPy and Pandas/Case Study 1/Assignment6.py
+++ NumPy and Pandas/Case Study 1/Assignment6.py
@@ -4,8 +4,25 @@
 # 8. Create a 10x10 array with random values and find the minimum and maximum values.
 # 9. Create a random vector of size 30 and find the mean value.
 
+# import numpy as np
+#
+# # arr=np.ndarray(shape=(3,4),strides=())
+# arr=np.reshape(np.arange(3*4),(3*4))
+# print(arr)
+
+
 import numpy as np
 
-# arr=np.ndarray(shape=(3,4),strides=())
-arr=np.reshape(np.arange(3*4),(3*4))
-print(arr)
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+arr2 = arr.copy()
+arr2=arr2.reshape(-1)
+i = 0
+for x in arr:
+    print(x)
+    for y in x:
+        print(y)
+        for z in y:
+            print(z)
+            arr2[i] = z
+            i = i + 1
+print(arr2)
