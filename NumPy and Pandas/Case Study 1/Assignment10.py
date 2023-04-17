68eda373b19fea1998955b313d563a22858f09ce
ManishPoojari <pmanish009@gmail.com>
2023-04-17 18:18:44 +0530

    Numpy-Pandas Case Study 1 -Solution 10 started

---
+++ NumPy and Pandas/Case Study 1/Assignment10.py
@@ -0,0 +1,5 @@
+# 10. Create numpy array having elements 0 to 10 And negate all the elements between 3 and 9
+import numpy as np
+
+arr= np.arange(11)
+print(arr)
---
+++ NumPy and Pandas/Case Study 1/Assignment11.py
@@ -0,0 +1,4 @@
+# 11. Create a random array of 3 rows and 3 columns and sort it according to 1st column, 2nd column or 3rd column.
+# 12. Create a four dimensions array get sum over the last two axis at once.
+# 13. Create a random array and swap two rows of an array.
+# 14. Create a random matrix and Compute a matrix rank.
