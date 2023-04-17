4b8e93a5f63acbaca3ab5eb11f77b4528f1f9e55
ManishPoojari <pmanish009@gmail.com>
2023-04-17 18:40:51 +0530

    Numpy-Pandas Case Study 1 -Solution 11 completed

--- NumPy and Pandas/Case Study 1/Assignment10.py
+++ NumPy and Pandas/Case Study 1/Assignment10.py
@@ -1,5 +1,11 @@
 # 10. Create numpy array having elements 0 to 10 And negate all the elements between 3 and 9
 import numpy as np
 
-arr= np.arange(11)
+arr = np.arange(11)
 print(arr)
+arr2 = np.negative(arr[2])
+for x in range(len(arr)):
+    if 3 < x < 9:
+        arr[x] = np.negative(arr[x])
+
+print("\n\nOutput with negeted value below\n", arr)
--- NumPy and Pandas/Case Study 1/Assignment11.py
+++ NumPy and Pandas/Case Study 1/Assignment11.py
@@ -1,4 +1,7 @@
 # 11. Create a random array of 3 rows and 3 columns and sort it according to 1st column, 2nd column or 3rd column.
-# 12. Create a four dimensions array get sum over the last two axis at once.
-# 13. Create a random array and swap two rows of an array.
-# 14. Create a random matrix and Compute a matrix rank.
+import numpy as np
+
+arr=np.random.randint(100,size=(3,3))
+print(arr)
+arr2=np.sort(arr,axis=0)
+print("\n\nOutput with vertical sorted value below\n", arr2)
