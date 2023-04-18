d84fe38c659e14394f84c9f451c22512a7908833
MspDx <pmanish009@gmail.com>
2023-04-18 15:51:24 +0530

    Numpy & Panda Case Study1-Assignment13 completed

--- NumPy and Pandas/Case Study 1/Assignment13.py
+++ NumPy and Pandas/Case Study 1/Assignment13.py
@@ -1,1 +1,14 @@
 # 13. Create a random array and swap two rows of an array.
+import numpy as np
+
+arr = np.random.randint(100, size=(2, 2))
+print("original array\n", arr, "\n")
+# long manual swap
+arr2 = arr.copy()
+arr[0, 0] = arr[1, 0]
+arr[0, 1] = arr[1, 1]
+arr[1, 0] = arr2[0, 0]
+arr[1, 1] = arr2[0, 1]
+arr3 = np.flipud(arr2)  # short inbuilt function to flip rows
+print("output of long version \n", arr, "\n")
+print("output of short inbuilt flipud() version \n", arr3, "\n")
